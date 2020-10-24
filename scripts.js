73c96b91bedf25ac2937914fc1ffa28b3e48e209
roi_hibou <ellisvoorhies@gmail.com>
2020-10-24 13:07:35 -0700

    Changed color scheme.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
+//Menu buttom animation
 function menuAnimation(x) {
   x.classList.toggle("change");
 }
 
+//Open Navbar
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
+//Close Navbar
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
 
+//Navbar disapears on scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
