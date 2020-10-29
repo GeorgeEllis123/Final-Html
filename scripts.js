915ff51078af19c1c2da63fefffd23f2ef1a003a
roi_hibou <ellisvoorhies@gmail.com>
2020-10-28 19:37:28 -0700

    Updated navbar.

--- scripts.js
+++ scripts.js
@@ -3,16 +3,25 @@ function menuAnimation(x) {
   x.classList.toggle("change");
 }
 
-//Open Navbar
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-//Close Navbar
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+//Opens and Closes Navbar
+var sideNavOpen = 0;
+function changeNav() {
+  var width = window.innerWidth;
+  if (sideNavOpen == 0) {
+    if (width > 570) {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+    else {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+    sideNavOpen = 1
+  }
+  else if (sideNavOpen == 1) {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    sideNavOpen = 0;
+  }
 }
 
 //Navbar disapears on scroll
